948a3f048f8eaac07584cc3b3ed74f5a0d491b42
nigel wareham <nigel.wareham@freightways.co.nz>
2017-09-13 01:51:00 +0000

    fix

--- test/BookingsUnitTest/BookingTest.cs
+++ test/BookingsUnitTest/BookingTest.cs
@@ -9,7 +9,7 @@ namespace BookingUnitTest
         [InlineData("nigel", true)]
         [InlineData("felix", true)]
         [InlineData("mike", true)]
-        [InlineData("bill", true)]
+        [InlineData("bill", false)]
         public void ValidUserId(string userId, bool expectedResult)
         {
             bool testValue = userId.Equals("nigel", StringComparison.OrdinalIgnoreCase) || userId.Equals("felix", StringComparison.OrdinalIgnoreCase) || userId.Equals("mike", StringComparison.OrdinalIgnoreCase);
