b2982269dec592c381f718fb8f35af50071f8e15
fwblue <31905525+fwblue@users.noreply.github.com>
2017-09-13 15:47:14 +1200

    Update Program.cs

--- src/Bookings/Program.cs
+++ src/Bookings/Program.cs
@@ -20,7 +20,7 @@ namespace Bookings
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://*:5001")
+                .UseUrls("http://*:80")
                 .UseIISIntegration()
                 .Build();
     }
